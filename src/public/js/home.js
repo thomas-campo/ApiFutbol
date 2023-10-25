9d8ac88b87345de9bd9a20918a8fdef44f7729bf
Thomas campo <thomascampocood@gmail.com>
2023-10-25 00:57:40 -0300

    first commit

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,20 +1,18 @@
-const API_URL = "https://api.football-data.org/v4/matches?X-Auth-Token=b337f2e7aaa141f38ab6fd54909b7e44";
+const API_URL = "https://api.football-data.org/v4/matches";
+const OPTIONS "http://api.football-data.org/v1/competitions/355/leagueTable";
 
 fetch(API_URL,{
-    method: "GET"
-    // headers: {
-    //     "X-Auth-Token": "b337f2e7aaa141f38ab6fd54909b7e44",
-    //     "Access-Control-Allow-Origin" : "*",
-    //     'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
-    //     "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
-    //     "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    // }
+    method: OPTIONS,
+    headers: {
+        'X-Auth-Token' : "b337f2e7aaa141f38ab6fd54909b7e44",
+        'authToken':"b337f2e7aaa141f38ab6fd54909b7e44"
+    }
 })
     .then(resp => resp.json())
     .then(data =>{
         console.log(data,"data")
     })
-    .catch(error => console.log('Authorization failed : ' + error.message + error));
+    .catch(error => console.log(error.message));
 
 // fetch("https://api.thecatapi.com/v1/images/search")
 //     .then(resp => resp.json())
